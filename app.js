27a487fce211a37ba80c855260fdcb125ffee393
pedrohrqphp <pedrohrq2005@gmail.com>
2023-05-04 21:25:22 -0300

    Lição Pronta

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 function logon() {
-   // const das = [{id:1, email:"teste@mail.com"}]
     
     const dados = [
-        {id:1, email:"Pedro", senha:"123"},
-        {id:2, email:"Nick", senha:"123"},
-        {id:3, email:"Ally", senha:"123"}
+        {id:1, email:"root", senha:"123"},
+        {id:2, email:"user1", senha:"123"},
+        {id:3, email:"user2", senha:"123"}
     ]
     
-
    return dados
 }
-
-
 
 function login(email, senha) {
     
     const user = logon()
-    
-   // alert(email + ":" + senha)
-      
+
     for(let i = 0; i < user.length; i++) {
     
         if (email == user[i].email && senha == user[i].senha) {
-            //alert('Você logou!')
-            window.location.href="http://www.google.pt"
+            
             window.location.href="dados.html"
+            alert('Você logou!')
+            
             break
-        } else {
+
+        } else if (email != user[i].email && senha != user[i].senha) {
+            
             alert('ERRO!')
+            
+            break
         }
     }
-   
 }
 
  
